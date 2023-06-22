6b25aa48c6a2172265d148651ef02a728e3d1da2
Yashvardhan Singh <yashsingh4703@gmail.com>
2023-06-22 14:29:25 +0530

    added edits and added functionality

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+   All: () => true,
+   Active: (task) => !task.completed,
+   Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
+
 function App(props) {
-  
+  const [filter, setFilter] = useState("All");
   const [tasks, setTasks] = useState(props.tasks);
 
   function addTask(name) {
@@ -23,12 +33,24 @@ function App(props) {
   setTasks(updatedTasks);
   }
 
+  function editTask(id, newName) {
+    const editedTasks = tasks.map((task) => {
+      if(id === task.id){
+        return {...task, name: newName};
+      }
+      return task;
+  });
+  setTasks(editedTasks);
+  }
+
   function deleteTask(id){
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
   }
 
-  const tasklist = tasks.map((task) => (
+  const tasklist = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
     <Todo 
       id = {task.id}
       name = {task.name}
@@ -36,8 +58,18 @@ function App(props) {
       keys = {task.id}
       toggleTaskCompleted = {toggleTaskCompleted}
       deleteTask = {deleteTask}
+      editTask = {editTask}
     />
   ));
+
+  const filterlist = FILTER_NAMES.map((name) => (
+    <FilterButton 
+    key = {name}
+    name = {name}
+    isPressed = {name === filter}
+    setFilter = {setFilter}
+    />
+  ))
 
   const taskNoun = tasklist.length !== 1 ? "tasks" : "task";
   const headingTask = `${tasklist.length} ${taskNoun} remaining`;
@@ -47,9 +79,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterlist}
       </div>
       <h2 id="list-heading">{headingTask}</h2>
       <ul
